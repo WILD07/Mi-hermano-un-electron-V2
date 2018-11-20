4cdd59c4f332bd70bc2fe34dd302bb5d87ab25e5
WILD07 <seg.wild07@gmail.com>
2018-11-20 12:59:12 -0400

    v4

---
+++ Mi Hermano un Electrón/Assets/Scripts/Desactivar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Desactivar : MonoBehaviour {
+    public new Collider collider;
+    public GameObject Puerta;   
+
+    // Use this for initialization
+    void Start()
+    {
+        Puerta = GameObject.Find("Puerta");        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Puerta.SetActive(false);
+        }
+    }
+}
--- Mi Hermano un Electrón/Assets/Scripts/UIControl.cs
+++ Mi Hermano un Electrón/Assets/Scripts/UIControl.cs
@@ -2,33 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIControl : MonoBehaviour {
     public GameObject pause;
     
 	// Use this for initialization
 	void Start () {
-        pause = GameObject.Find("Pausa");
-        pause.gameObject.SetActive(false);
-        //pause = GetComponent<Image>();
-   
+           
     }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+    public void menu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void salir()
+    {
+        Debug.Log("Salir Del Juego");
+        Application.Quit();
+    }
+    public void Reniciar()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("SampleScene");
+    }
     public void Pause()
     {
-        if (pause==false)
-        {           
-            pause.SetActive(true);
-            Time.timeScale = 0f;
-        }
-        else if(pause==true)
+        if (Time.timeScale == 1)
         {
+            Time.timeScale = 0;
+            pause.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1;
             pause.SetActive(false);
-            Time.timeScale = 1.0f;
         }
     }
 }
